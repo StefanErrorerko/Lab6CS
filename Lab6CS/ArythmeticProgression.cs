01b687fadfe374779163001d1a7f910e1fcdebbc
StefanErrorerko <76735417+StefanErrorerko@users.noreply.github.com>
2022-11-25 21:32:57 +0200

    Add project files.

---
+++ Lab6CS/ArythmeticProgression.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab6CS
+{
+    public class ArythmeticProgression : Progression
+    {
+        public double a1 { get; init; }
+        public double d { get; init; }
+
+        public ArythmeticProgression(double a1, double d)
+        {
+            this.a1 = a1;
+            this.d = d;
+        }
+
+        public override double Element(int n)
+        {
+            return a1 + d * (n - 1);
+        }
+
+        public override void DisplayAll()
+        {
+            Console.WriteLine($"a1: { a1}");
+            Console.WriteLine($"d: { d}");
+        }
+
+    }
+}
---
+++ Lab6CS/Progression.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab6CS
+{
+    public class Progression
+    {
+        public virtual double Element(int n)
+        {
+            return n;
+        }
+        public virtual void DisplayAll()
+        {
+            Console.WriteLine("Nothing");
+        }
+    }
+}
